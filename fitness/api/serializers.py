808b0c09bc557871aba230f71f4d27a3775f1b4c
Vladut Cretu <vladutcretu98@gmail.com>
2024-12-27 16:26:45 +0200

    API branch: Add Sets API view

--- fitness/api/serializers.py
+++ fitness/api/serializers.py
@@ -2,7 +2,7 @@
 from rest_framework import serializers
 
 # App
-from fitness.models import MuscleGroup, Exercise, Workout
+from fitness.models import MuscleGroup, Exercise, Workout, WorkingSet
 
 
 class MuscleGroupSerializer(serializers.ModelSerializer):
@@ -31,7 +31,7 @@ class ExerciseSerializer(serializers.ModelSerializer):
 class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workout
-        fields = ['id', 'name', 'public', 'updated', 'created']
+        fields = ['id', 'name', 'exercises', 'public', 'updated', 'created']
     
     def validate(self, attrs):
         """Check if the user already has a workout for the specified date."""
@@ -48,4 +48,41 @@ class WorkoutSerializer(serializers.ModelSerializer):
 
         if existing_workouts.exists():
             raise serializers.ValidationError({"created": "You already have a workout created for this date. Try another date or delete the existing workout!"})
+        return attrs
+    
+
+class WorkingSetSerializer(serializers.ModelSerializer):
+    workout = serializers.IntegerField(write_only=True) # Accept workout id in POST
+    exercise = serializers.IntegerField(write_only=True) # Accept exercise id in POST
+    workout_details = MuscleGroupSerializer(read_only=True, source='workout')  # Return full workout details in GET
+    exercise_details = MuscleGroupSerializer(read_only=True, source='exercise')  # Return full exercise details in GET
+
+    class Meta:
+        model = WorkingSet
+        fields = ['id', 'workout', 'workout_details', 'exercise', 'exercise_details', 'type', 'weight', 'repetitions', 'distance', 'time', 'updated', 'created']
+
+    def validate(self, attrs):
+        """Check if the workout and exercise are related and valid (existing and owned by requested user)."""
+        user = self.context['request'].user
+        workout_id = attrs.get('workout')
+        exercise_id = attrs.get('exercise')
+
+        # Check if workout ID exists and is owned by requested user
+        try:
+            workout = Workout.objects.get(id=workout_id, user=user)
+        except Workout.DoesNotExist:
+            raise serializers.ValidationError({"workout": "User does not have the specified workout. Please specify an existing workout ID!"})
+
+        # Check if the exercise ID exists and is owned by requested user
+        try:
+            exercise = Exercise.objects.get(id=exercise_id, user=user)
+        except Exercise.DoesNotExist:
+            raise serializers.ValidationError({"exercise": "User does not have the specified exercise. Please specify an existing exercise ID!"})
+
+        # Check if workout and exercise is related (exercise is added to the workout)
+        if not workout.exercises.filter(id=exercise_id).exists():
+            raise serializers.ValidationError({"exercise": "The specified exercise is not associated with the specified workout. Please add exercise to the workout first!"})
+
+        attrs['workout'] = workout
+        attrs['exercise'] = exercise
         return attrs
--- fitness/api/views.py
+++ fitness/api/views.py
@@ -3,8 +3,8 @@ from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 
 # App
-from fitness.models import MuscleGroup, Exercise, Workout
-from .serializers import MuscleGroupSerializer, ExerciseSerializer, WorkoutSerializer
+from fitness.models import MuscleGroup, Exercise, Workout, WorkingSet
+from .serializers import MuscleGroupSerializer, ExerciseSerializer, WorkoutSerializer, WorkingSetSerializer
 from .permissions import IsOwner
 
 
@@ -44,6 +44,20 @@ class WorkoutAPIView(viewsets.ModelViewSet):
     def get_queryset(self):
         """Only allow access to appropriate (owned) object(s)"""
         return Workout.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """Set the user field to the currently authenticated (and requested) user."""
+        serializer.save(user=self.request.user)
+
+
+class WorkingSetAPIView(viewsets.ModelViewSet):
+    """API endpoint for the sets (CRUD operations)"""
+    serializer_class = WorkingSetSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        """Only allow access to appropriate (owned) object(s)"""
+        return WorkingSet.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         """Set the user field to the currently authenticated (and requested) user."""
